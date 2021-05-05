077c1f7a37e898bb78f3618ef6270d3937b3ff94
Vy0l3tH <vy0l3th@gmail.com>
2021-05-06 00:14:44 +0200

    layout

--- src/components/PatientsList.vue
+++ src/components/PatientsList.vue
@@ -10,29 +10,34 @@
       </b-col>
     </b-row>
     <b-row>
-<!-- filtre sur l'ensemble des données -->
-        <b-form-group
-          label-for="filter-input"
-        >
-          <b-input-group size="sm">
-            <b-form-input
-              id="filter-input"
-              v-model="filter"
-              type="search"
-              placeholder="Recherche"
+      <!-- filtre sur l'ensemble des données -->
+      <b-form-group label-for="filter-input">
+        <b-input-group size="sm">
+          <b-form-input
+            id="filter-input"
+            v-model="filter"
+            type="search"
+            placeholder="Recherche"
+          >
+          </b-form-input>
+          <b-input-group-append>
+            <b-button :disabled="!filter" @click="filter = ''"
+              >Effacer</b-button
             >
-            </b-form-input>
-            <b-input-group-append>
-              <b-button :disabled="!filter" @click="filter = ''">Effacer</b-button>
-            </b-input-group-append>
-          </b-input-group>
-        </b-form-group>
-<!-- fin du filtre -->
-
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+      <!-- fin du filtre -->
     </b-row>
     <b-row>
       <b-col cols="16" align-h="start">
-        <b-table striped hover  :items="patients" :fields="fieldsDef" :filter="filter" >
+        <b-table
+          striped
+          hover
+          :items="patients"
+          :fields="fieldsDef"
+          :filter="filter"
+        >
           <template #cell(plop)="data">
             <b-button
               class="btn btn-sm btn-info mr-2"
@@ -46,25 +51,25 @@
             >
               Delete
             </b-button>
-            <b-button  class="btn btn-sm btn-info" v-b-modal.modal-1>Send form</b-button>
-
-       
+            <b-button class="btn btn-sm btn-info" v-b-modal.modal-1
+              >Send form</b-button
+            >
           </template>
         </b-table>
       </b-col>
     </b-row>
-     <b-modal id="modal-1" title="Send form">
-           <b-form-select v-model="selectedForm">
-            <b-select-option
-              v-for="form in formsList"
-              v-bind:key="form.id"
-              v-bind:value="form"
-            >
-              {{ form.name }}
-            </b-select-option>
-            <!-- objet littéral en ligne -->
-          </b-form-select>
-        </b-modal>
+    <b-modal id="modal-1" title="Send form">
+      <b-form-select v-model="selectedForm">
+        <b-select-option
+          v-for="form in formsList"
+          v-bind:key="form.id"
+          v-bind:value="form"
+        >
+          {{ form.name }}
+        </b-select-option>
+        <!-- objet littéral en ligne -->
+      </b-form-select>
+    </b-modal>
   </b-container>
 </template>
 <script>
@@ -157,9 +162,9 @@ export default {
       currentPatient: null,
       currentIndex: -1,
       title: "",
-     formsList: [],
+      formsList: [],
       filter: null,
-      selectedForm:""
+      selectedForm: "",
     };
   },
   methods: {
@@ -173,7 +178,8 @@ export default {
         .catch((e) => {
           console.log(e);
         });
-    }, retrieveForms() {
+    },
+    retrieveForms() {
       FormDataService.getAll()
         .then((response) => {
           console.log(response.data);
